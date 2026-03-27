20f27b85bc9b725325fc64eb5f2cef2e9d36304d
khurtsidzenugzari01-cell <khurtsidzenugzari01@gmail.com>
2026-03-28 01:07:49 +0400

    sapinalo-proecti

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let navbar = document.getElementById("navigeishen");
+let burgerbar = document.getElementById("burger");
+
+burgerbar.addEventListener("click",function () {
+ navbar.classList.toggle("active");
+});
